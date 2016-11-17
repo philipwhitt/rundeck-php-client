40f5a4c54220e0afe2246b8073171a0d0b9cfef2
Victor Gryshko <stalin1945@gmail.com>
2016-11-18 01:34:03 +0200

    Added testcase for PR #1 Refactored tests to use data providers

--- test/Rundeck/RundeckClientTest.php
+++ test/Rundeck/RundeckClientTest.php
@@ -7,77 +7,179 @@ use GuzzleHttp\Subscriber\Mock;
 use GuzzleHttp\Message\Response;
 use GuzzleHttp\Stream\Stream;
 
-class RundeckClientTest extends \PHPUnit_Framework_TestCase {
+class RundeckClientTest extends \PHPUnit_Framework_TestCase
+{
 
-	public function setup() {
-		$this->client = new RundeckClientStub('http://localhost:4440', 'admin', 'admin', '2.6.1');
-	}
+    /**
+     * @var RundeckClientStub
+     */
+    protected $client;
 
-	public function testGetProjects() {
-		// given
-		$body = Stream::factory(file_get_contents(__DIR__.'/projects.xml'));
+    public function setup()
+    {
+        $this->client = new RundeckClientStub('http://localhost:4440', 'admin', 'admin', '2.6.1');
+    }
 
-		$mock = new Mock([new Response(200, [], $body)]);
-		$this->client->getHttpClient()->getEmitter()->attach($mock);
+    /**
+     * @param $file
+     * @param $expectedProjects
+     * @dataProvider getDataForTestGetProjects
+     */
+    public function testGetProjects($file, $expectedProjects)
+    {
+        // given
+        $body = Stream::factory(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $file));
 
-		// when
-		$projects = $this->client->getProjects();
+        $mock = new Mock([new Response(200, [], $body)]);
+        $this->client->getHttpClient()->getEmitter()->attach($mock);
 
-		// then
-		$this->assertEquals(count($projects), 2);
+        // when
+        /** @var api\Project[] $projects */
+        $projects = $this->client->getProjects();
 
-		$this->assertEquals($projects[0], (new api\Project)
-			->setName("Project 1")
-			->setDescription("Lorem Ipsum")
-		);
+        // then
+        $this->assertEquals(count($expectedProjects), count($projects));
 
-		$this->assertEquals($projects[1], (new api\Project)
-			->setName("Project 2")
-			->setDescription("Lorem Ipsum 2")
-		);
-	}
+        foreach ($projects as $project) {
+            $this->assertInstanceOf('Rundeck\\Api\\Project', $project);
+            list($key, $expectedProject) = each($expectedProjects);
+            $this->assertEquals($expectedProject['name'], $project->getName());
+            $this->assertEquals($expectedProject['description'], $project->getDescription());
+        }
+    }
 
-	public function testGetJobs() {
-		// given
-		$body = Stream::factory(file_get_contents(__DIR__.'/jobs.xml'));
+    /**
+     * @param string $file
+     * @param string $project
+     * @param array $expectedJobs
+     * @dataProvider getDataForTestGetJobs
+     */
+    public function testGetJobs($file, $project, $expectedJobs)
+    {
+        // given
+        $body = Stream::factory(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $file));
 
-		$mock = new Mock([new Response(200, [], $body)]);
-		$this->client->getHttpClient()->getEmitter()->attach($mock);
+        $mock = new Mock([new Response(200, [], $body)]);
+        $this->client->getHttpClient()->getEmitter()->attach($mock);
 
-		// when
-		$jobs = $this->client->getJobs('test');
+        // when
+        /** @var api\Job[] $jobs */
+        $jobs = $this->client->getJobs($project);
 
-		// then
-		$this->assertEquals(count($jobs), 2);
+        // then
+        $this->assertEquals(count($expectedJobs), count($jobs));
 
-		$this->assertEquals($jobs[0], (new api\Job)
-			->setId("2342345-ac1d-43b2-b00f-uflkJ234")
-			->setName("some-job-name")
-			->setProject("test")
-			->setDescription("This is a job for some tests")
-		);
+        foreach ($jobs as $job) {
+            $this->assertInstanceOf('Rundeck\\Api\\Job', $job);
+            list($key, $expectedJob) = each($expectedJobs);
+            $this->assertEquals($expectedJob['id'], $job->getId());
+            $this->assertEquals($expectedJob['name'], $job->getName());
+            $this->assertEquals($expectedJob['project'], $job->getProject());
+            $this->assertEquals($expectedJob['description'], $job->getDescription());
+        }
+    }
 
-		$this->assertEquals($jobs[1], (new api\Job)
-			->setId("234lkjfou-6c4a-4eff-b31f-234kljsdf9Ijlk")
-			->setName("another-job-name")
-			->setProject("test")
-			->setDescription("Lorem Ipsum")
-		);
-	}
+    /**
+     * @param string $project
+     * @param string $file
+     * @dataProvider getDataForTestExportProject
+     */
+    public function testExportJobs($project, $file)
+    {
+        // given
+        $xmlA = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $file);
+        $body = Stream::factory($xmlA);
 
-	public function testExportJobs() {
-		// given
-		$xmlA = file_get_contents(__DIR__.'/exportJobs.xml');
-		$body = Stream::factory($xmlA);
+        $mock = new Mock([new Response(200, [], $body)]);
+        $this->client->getHttpClient()->getEmitter()->attach($mock);
 
-		$mock = new Mock([new Response(200, [], $body)]);
-		$this->client->getHttpClient()->getEmitter()->attach($mock);
+        // when
+        $xmlB = $this->client->exportJobs($project);
 
-		// when
-		$xmlB = $this->client->exportJobs('test');
+        // then
+        $this->assertEquals($xmlB, $xmlA);
+    }
 
-		// then
-		$this->assertEquals($xmlB, $xmlA);
-	}
+    /**
+     * @return array
+     */
+    public function getDataForTestGetProjects()
+    {
+        return [
+            [
+                'projectsMultiple.xml',
+                [
+                    [
+                        'name' => 'Project 1',
+                        'description' => 'Lorem Ipsum',
+                    ],
+                    [
+                        'name' => 'Project 2',
+                        'description' => 'Lorem Ipsum 2',
+                    ],
+                ]
+            ],
+            [
+                'projectsSingle.xml',
+                [
+                    [
+                        'name' => 'Project 1',
+                        'description' => 'Lorem Ipsum',
+                    ],
+                ],
+            ],
+        ];
+    }
 
+    /**
+     * @return array
+     */
+    public function getDataForTestGetJobs()
+    {
+        return [
+            [
+                'jobsMultiple.xml',
+                'test',
+                [
+                    [
+                        'id' => '2342345-ac1d-43b2-b00f-uflkJ234',
+                        'name' => 'some-job-name',
+                        'project' => 'test',
+                        'description' => 'This is a job for some tests',
+                    ],
+                    [
+                        'id' => '234lkjfou-6c4a-4eff-b31f-234kljsdf9Ijlk',
+                        'name' => 'another-job-name',
+                        'project' => 'test',
+                        'description' => 'Lorem Ipsum',
+                    ],
+                ]
+            ],
+            [
+                'jobsSingle.xml',
+                'test',
+                [
+                    [
+                        'id' => '2342345-ac1d-43b2-b00f-uflkJ234',
+                        'name' => 'some-job-name',
+                        'project' => 'test',
+                        'description' => 'This is a job for some tests',
+                    ],
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getDataForTestExportProject()
+    {
+        return [
+            [
+                'test',
+                'exportJobs.xml',
+            ],
+        ];
+    }
 }
